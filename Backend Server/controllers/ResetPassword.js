d4ec02bfab68f821e34963c1d96ad8a54df5b5f4
Lux <luxprajapati81@gmail.com>
2024-06-16 10:20:15 +0530

    Update Password URL Updated

--- Backend Server/controllers/ResetPassword.js
+++ Backend Server/controllers/ResetPassword.js
@@ -27,7 +27,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
     // console.log("DETAILS", updatedDetails);
 
-    const url = `http://localhost:3000/update-password/${token}`;
+    const url = `http://codenotion-luxprajapati.netlify.app/update-password/${token}`;
 
     await mailSender(
       email,
