ce309af9489760255cb98a1440d57680231df719
Lux <luxprajapati81@gmail.com>
2024-06-16 13:09:57 +0530

    URL error Updated

--- Backend Server/controllers/ResetPassword.js
+++ Backend Server/controllers/ResetPassword.js
@@ -27,7 +27,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
     // console.log("DETAILS", updatedDetails);
 
-    const url = `https://code-notion-lux.vercel.app//update-password/${token}`;
+    const url = `https://code-notion-lux.vercel.app/update-password/${token}`;
 
     await mailSender(
       email,
--- Backend Server/index.js
+++ Backend Server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://code-notion-lux.vercel.app/",
+    origin: "https://code-notion-lux.vercel.app",
     credentials: true,
   })
 );
